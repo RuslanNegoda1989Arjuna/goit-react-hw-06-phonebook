b0d405900d0d6a90fefdaa86e18fde17a62d2cc8
Ruslan Negoda <iarjuna108@gmail.com>
2022-12-08 21:33:50 +0200

    1

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,12 +4,12 @@ import { createAction, createReducer } from '@reduxjs/toolkit';
 // export const addContact = createAction('Contact/addContact');
 // export const deleteContact = createAction('Contact/deleteContact');
 
-const contactsReducer = createReducer([], {
-  // [addContact]: (state, action) => state.push(action.payload),
-  // [deleteContact]: (state, action, idContact) =>
-  //   state.filter(contact => contact.id !== idContact),
-});
+// const contactsReducer = createReducer([], {
+//   [addContact]: (state, action) => state.push(action.payload),
+//   [deleteContact]: (state, action, idContact) =>
+//     state.filter(contact => contact.id !== idContact),
+// });
 
-export const store = configureStore({
-  contacts: contactsReducer,
-});
+// export const store = configureStore({
+//   contacts: contactsReducer,
+// });
