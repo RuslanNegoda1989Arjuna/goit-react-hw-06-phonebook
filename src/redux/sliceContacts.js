c14e233c6fa87005b8ac6102152820acffdb278a
Ruslan Negoda <iarjuna108@gmail.com>
2022-12-09 16:25:12 +0200

    start slice Redux

---
+++ src/redux/sliceContacts.js
@@ -0,0 +1,30 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+const contactsInitualeState = [
+  { id: nanoid(5), name: 'Лунная Роза', number: '071-0-232' },
+  { id: nanoid(5), name: 'Hermione Kline', number: '645-17-19' },
+  { id: nanoid(5), name: 'Eden Clements', number: '443-89-12' },
+  { id: nanoid(5), name: 'Annie Copeland', number: '459-12-56' },
+];
+
+const contactsSlice = createSlice({
+  // Ім'я слайсу
+  name: 'contacts',
+  // Початковий стан редюсера слайсу
+  initialState: contactsInitualeState,
+  // Об'єкт редюсерів
+  reducers: {
+    addContact(state, action) {
+      state.push(action.payload);
+    },
+    deleteContact(state, action) {
+      return state.filter(el => el.id !== action.payload);
+    },
+  },
+});
+
+// Генератори екшенів
+const { addContact, deleteContact } = contactsSlice.actions;
+
+// Редюсер слайсу
+const contactsReducer = contactsSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { createAction, createReducer } from '@reduxjs/toolkit';
 
-// export const addContact = createAction('Contact/addContact');
-// export const deleteContact = createAction('Contact/deleteContact');
-
-// const contactsReducer = createReducer([], {
-//   [addContact]: (state, action) => state.push(action.payload),
-//   [deleteContact]: (state, action, idContact) =>
-//     state.filter(contact => contact.id !== idContact),
-// });
-
-// export const store = configureStore({
-//   contacts: contactsReducer,
-// });
+export const store = configureStore({
+  reducer: {
+    contacts: [],
+    filter: '',
+  },
+});
